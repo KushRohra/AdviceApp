03f82229743f298ea0884f47025f12df18ef21a8
KushRohra <iec2017024@iiita.ac.in>
2020-07-17 18:13:36 +0530

    Updates

---
+++ precache-manifest.b686e792ab2df7ea31a31493c7a09fca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a405b3e577b4844a75dd9b38ab24ec06",
+    "url": "/KushRohra/AdviceApp/index.html"
+  },
+  {
+    "revision": "3cf9a58b9577efe426ea",
+    "url": "/KushRohra/AdviceApp/static/css/main.0206c421.chunk.css"
+  },
+  {
+    "revision": "e6a4cf4020b764b39a74",
+    "url": "/KushRohra/AdviceApp/static/js/2.6d2c2c71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KushRohra/AdviceApp/static/js/2.6d2c2c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf9a58b9577efe426ea",
+    "url": "/KushRohra/AdviceApp/static/js/main.b7d5e71b.chunk.js"
+  },
+  {
+    "revision": "e851dd4e511c360ac4cf",
+    "url": "/KushRohra/AdviceApp/static/js/runtime-main.5e7183ce.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/KushRohra/AdviceApp/static/media/city.91c109fc.jpg"
+  }
+]);
